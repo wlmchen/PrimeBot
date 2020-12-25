2f95e7d4415562ad7cf7ae64a2abadb705f3477d
William Chen <wlmchen16@gmail.com>
2020-12-24 16:27:27 -0800

    minor fix

--- cogs/help.py
+++ cogs/help.py
@@ -7,7 +7,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        embedHelp = discord.Embed(title="Help", description="Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>test (text)`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`unban (member)`\n\nThis Bot is Open Source!\https://gitlab.com/pryme-svg/primebot", color=0x282828)
+        embedHelp = discord.Embed(title="Help", description="Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>test (text)`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`unban (member)`\n\nThis Bot is Open Source! https://gitlab.com/pryme-svg/primebot", color=0x282828)
         embedHelp.set_footer(text="Made by GNU/PrimeTime09#1847, Running on Debian GNU/Linux")
         await ctx.send(embed = embedHelp)
 
