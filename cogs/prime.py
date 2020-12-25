24c5bb819ce4be392885f580b75f109f8be92967
William Chen <wlmchen16@gmail.com>
2020-12-24 16:50:16 -0800

    remove embeds

--- cogs/prime.py
+++ cogs/prime.py
@@ -7,7 +7,7 @@ class Prime(commands.Cog):
 
     @commands.command()
     async def prime(self, ctx):
-        await ctx.send("Thanks for using my bot! Feel free to browse my github at https://github.com/pryme-svg\nOr check out my gitlab at https://gitlab.com/pryme-svg\nThis bot is open source, please star the repository! https://gitlab.com/pryme-svg/primebot/")
+        await ctx.send("Thanks for using my bot! Feel free to browse my github at <https://github.com/pryme-svg>\nOr check out my gitlab at <https://gitlab.com/pryme-svg>\nThis bot is open source, please star the repository! <https://gitlab.com/pryme-svg/primebot/>")
 
 def setup(bot): 
     bot.add_cog(Prime(bot) 
