190b06cd67231561efd42dd4853fe3b611aa59c2
pryme-svg <edoc.www@gmail.com>
2020-12-28 08:43:30 -0800

    server prefixes

--- bot.py
+++ bot.py
@@ -5,16 +5,55 @@ import json
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
-from discord.ext.commands import MissingPermissions
+from discord.ext.commands import has_permissions, MissingPermissions
 import traceback
 import logging
+
+def get_prefix(bot, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix = get_prefix)
 bot.remove_command('help')
 
+@bot.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = '>'
+    
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+
+@bot.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@bot.command()
+@commands.has_permissions(manage_roles=True)
+async def changeprefix(ctx, prefix):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+    await ctx.send(f'Prefix changed to: {prefix}')
 
 # load cogs
 
--- cogs/help.py
+++ cogs/help.py
@@ -1,4 +1,5 @@
 import discord
+import json
 from discord.ext import commands
 
 class Help(commands.Cog):
@@ -7,7 +8,8 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        embedHelp = discord.Embed(title="Help", description="Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>test (text)`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`unban (member)`\n`poll question item1 item2 ...`\n`xkcd`\n`xkcd latest`\n`xkcd n <number`\n`>sys`\n`>info`\n\nThis Bot is Open Source! https://gitlab.com/pryme-svg/primebot", color=0x282828)
+
+        embedHelp = discord.Embed(title="Help", description="Default Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>test (text)`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`unban (member)`\n`poll question item1 item2 ...`\n`xkcd`\n`xkcd latest`\n`xkcd n <number`\n`>sys`\n`>info`\n`changeprefix (prefix)`\n\nThis Bot is Open Source! https://gitlab.com/pryme-svg/primebot", color=0x282828)
         embedHelp.set_footer(text="Made by GNU/PrimeTime09#1847, Running on Debian GNU/Linux")
         await ctx.send(embed = embedHelp)
 
