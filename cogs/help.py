5de06d1de5e0afff3574a6adbb48e7ed947bacdf
pryme-svg <edoc.www@gmail.com>
2020-12-29 19:04:52 -0800

    update help

--- cogs/help.py
+++ cogs/help.py
@@ -9,7 +9,7 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
 
-        embedHelp = discord.Embed(title="Help", description="Default Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`unban (member)`\n`poll question item1 item2 ...`\n`xkcd`\n`xkcd latest`\n`xkcd n <number`\n`>sys`\n`>info`\n`changeprefix (prefix)`\n`flip`\n`githead`\n`gitlog`\n\nThis Bot is Open Source! https://gitlab.com/pryme-svg/primebot", color=0x282828)
+        embedHelp = discord.Embed(title="Help", description="Default Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`unban (member)`\n`poll question item1 item2 ...`\n`xkcd`\n`xkcd latest`\n`xkcd n <number`\n`>sys`\n`>info`\n`changeprefix (prefix)`\n`flip`\n`githead`\n`gitlog`\n`distro (distro)`\n\nThis Bot is Open Source! https://gitlab.com/pryme-svg/primebot", color=0x282828)
         embedHelp.set_footer(text="Made by GNU/PrimeTime09#1847, Running on Debian GNU/Linux")
         await ctx.send(embed = embedHelp)
 
