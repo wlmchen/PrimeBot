b0ae752d63ccdce7a95ca0bd1cae0396eb4a4ef7
William Chen <wlmchen16@gmail.com>
2020-12-29 16:48:54 -0800

    fix git (squashed)
    fix git
    
    fix git
    
    fix git
    
    fix git
    
    fix git
    
    update git
    
    fix git
    
    fix git
    
    fix git
    
    add git cooldown
    
    update help
    
    diff
    
    diff for log

--- cogs/git.py
+++ cogs/git.py
@@ -8,11 +8,30 @@ class Git(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
     async def githead(self, ctx):
-        head = subprocess.run(['git', '--no-pager', 'show', 'HEAD'], stdout-subprocess.PIPE)
-        await ctx.send(head)
+        head = subprocess.run(['git', '--no-pager', 'show', 'HEAD'], stdout=subprocess.PIPE)
+        if len(head.stdout.decode('utf-8')) > 6000:
+            mystr = head.stdout.decode('utf-8')
+            mystr = mystr[0:5000]
+        else:
+            mystr = head.stdout.decode('utf-8')
+        mystr = "```diff\n" + mystr + "```"
+        embedLog = discord.Embed(title="Git Head", description=mystr)
+        await ctx.send(embed=embedLog)
 
     @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
     async def gitlog(self, ctx):
-        log = subprocess.run(['git', '--no-pager', 'log'], stdout-subprocess.PIPE)
-        await ctx.send(log)
+        log = subprocess.run(['git', '--no-pager', 'log'], stdout=subprocess.PIPE)
+        if len(log.stdout.decode('utf-8')) > 6000:
+            mystr = log.stdout.decode('utf-8')
+            mystr = mystr[0:2048]
+        else:
+            mystr = log.stdout.decode('utf-8')
+            mystr = "```\n" + mystr + "```"
+        embedLog = discord.Embed(title="Git Log(truncated)", description=mystr)
+        await ctx.send(embed=embedLog)
+
+def setup(bot):
+    bot.add_cog(Git(bot))
