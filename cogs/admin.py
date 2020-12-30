34f8c7251ca7d4e44497fc0a838d8718d9187708
pryme-svg <edoc.www@gmail.com>
2020-12-29 16:38:18 -0800

    e

--- cogs/admin.py
+++ cogs/admin.py
@@ -46,7 +46,7 @@ class Admin(commands.Cog):
     @commands.is_owner()
     async def restartBot(self, ctx):
         await ctx.send(":robot: Bot is restarting")
-        subprocess.call(["git", "pull --rebase"])
+        subprocess.call(["git", "pull", "--rebase"])
         await ctx.bot.logout()
         await login(TOKEN, bot=True)
 
