28e3587c419a598e01b76d9de630edfee10f2ce8
William Chen <wlmchen16@gmail.com>
2021-01-02 11:01:02 -0800

    avatar

---
+++ cogs/avatar.py
@@ -0,0 +1,17 @@
+import discord 
+from discord.ext import commands
+
+class Avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def avatar(self, ctx, *,  avamember):
+        avamember await convert.convert(ctx, avamember)
+        userAvatarUrl = avamember.avatar_url
+        embedAvatar = discord.Embed(title=str(avamember), description = '')
+        embedAvatar.set_image(url=userAvatarUrl)
+        await ctx.send(embed=embedAvatar)
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
