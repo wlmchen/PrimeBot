db02ab960a35cb8db20872e41495e528416346bb
William Chen <wlmchen16@gmail.com>
2021-01-05 11:07:10 -0800

    readme update and fix whois

--- cogs/whois.py
+++ cogs/whois.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 class Whois(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-    
+
     @staticmethod
     def _getRoles(roles):
         string = ''
@@ -36,11 +36,10 @@ class Whois(commands.Cog):
             else:
                 fullName = member
             embed.add_field(name=member.name, value=fullName, inline=False)
-            embed.add_field(name='Joined Discord on:', value='{}\n)'.format(member.created_at.strftime('%d.%m.%Y')), inline=True)
-            embed.add_field(name='Joined Server on', value='{}\n)'.format(member.joined_at.strftime('%d.%m.%Y')), inline=True)
+            embed.add_field(name='Joined Discord on:', value='{}\n'.format(member.created_at.strftime('%d.%m.%Y')), inline=True)
+            embed.add_field(name='Joined Server on', value='{}\n'.format(member.joined_at.strftime('%d.%m.%Y')), inline=True)
             embed.add_field(name='Avatar Link', value=member.avatar_url, inline=False)
             embed.add_field(name='Roles', value=self._getRoles(member.roles), inline=True)
-            embed.add_field(name='Status', value=member.status, inline=True)
             await ctx.send(embed=embed)
         else:
             msg = 'You have not specified a user!'
