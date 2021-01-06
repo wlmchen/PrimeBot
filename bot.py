4b8aff8208adb35cc5d1ba8a9049990f4d13edf1
pryme-svg <edoc.www@gmail.com>
2021-01-06 09:56:12 -0800

    remove server prefixes

--- bot.py
+++ bot.py
@@ -4,62 +4,16 @@ import json
 from dotenv import load_dotenv
 from discord.ext import commands
 
-
-def get_prefix(bot, message):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    return prefixes[str(message.guild.id)]
-
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix=get_prefix)
+bot = commands.Bot(command_prefix=">")
 bot.remove_command('help')
-
-
-@bot.event
-async def on_guild_join(guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(guild.id)] = '>'
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-
-@bot.event
-async def on_guild_remove(guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id))
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-
-@bot.command()
-@commands.has_permissions(manage_roles=True)
-async def changeprefix(ctx, prefix):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-    await ctx.send(f'Prefix changed to: {prefix}')
 
 # load cogs
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-
-# command cooldown
 
 bot.run(TOKEN)
