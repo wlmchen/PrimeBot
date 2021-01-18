5fae004615c5473afa5f4a919abcf9324209466e
pryme-svg <edoc.www@gmail.com>
2021-01-18 13:09:03 -0800

    encodings

---
+++ primebot/cogs/encoding.py
@@ -0,0 +1,60 @@
+from discord.ext import commands
+import base64
+
+
+class Encoding(commands.cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def b64encode(self, ctx, *, arg):
+        to_encode = arg
+        encoded = base64.b64encode(bytes(to_encode, 'utf-8'))
+        await ctx.send(encoded)
+
+    @commands.command()
+    async def b64decode(self, ctx, *, arg):
+        to_decode = arg
+        decoded = base64.b64decode(to_decode)
+        await ctx.send(decoded)
+
+    @commands.command()
+    async def b16encode(self, ctx, *, arg):
+        to_encode = arg
+        encoded = base64.b16encode(bytes(to_encode, 'utf-8'))
+        await ctx.send(encoded)
+
+    @commands.command()
+    async def b16decode(self, ctx, *, arg):
+        to_decode = arg
+        decoded = base64.b16decode(to_decode)
+        await ctx.send(decoded)
+
+    @commands.command()
+    async def a85encode(self, ctx, *, arg):
+        to_encode = arg
+        encoded = base64.a85encode(bytes(to_encode, 'utf-8'))
+        await ctx.send(encoded)
+
+    @commands.command()
+    async def a85decode(self, ctx, *, arg):
+        to_decode = arg
+        decoded = base64.a85decode(to_decode)
+        await ctx.send(decoded)
+
+    @commands.command()
+    async def b85encode(self, ctx, *, arg):
+        to_encode = arg
+        encoded = base64.b85encode(bytes(to_encode, 'utf-8'))
+        await ctx.send(encoded)
+
+    @commands.command()
+    async def b85decode(self, ctx, *, arg):
+        to_decode = arg
+        decoded = base64.b85decode(to_decode)
+        await ctx.send(decoded)
+
+
+def setup(bot):
+    bot.add_cog(Encoding(bot))
--- primebot/cogs/help.py
+++ primebot/cogs/help.py
@@ -9,7 +9,7 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
 
-        embedHelp = discord.Embed(title="Help", description="Default Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`>unban (member)`\n`>poll question item1 item2 ...`\n`>xkcd`\n`>xkcd latest`\n`>xkcd n <number>`\n`>sys`\n`>info`\n`>flip`\n`>distro (distro)`\n`>distro random`\n`>apod`\n`>apod (date)`\n`>archwiki (search)`\n`>define (term)`\n`>figlet (text)`\n`>invite`\n\nThis Bot is Open Source! Check out the repo [here](https://gitlab.com/pryme-svg/primebot)", color=0x282828)
+        embedHelp = discord.Embed(title="Help", description="Default Prefix: >\nCommands:\n`>ping`\n`>quote`\n`>prime`\n`>ban`\n`>kick`\n`>roll`\n`>8ball (question)`\n`>clear (amount)`\n`>unban (member)`\n`>poll question item1 item2 ...`\n`>xkcd`\n`>xkcd latest`\n`>xkcd n <number>`\n`>sys`\n`>info`\n`>flip`\n`>distro (distro)`\n`>distro random`\n`>apod`\n`>apod (date)`\n`>archwiki (search)`\n`>define (term)`\n`>figlet (text)`\n`>invite`\n`>b64encode`\n`>b64decode`\n\nThis Bot is Open Source! Check out the repo [here](https://gitlab.com/pryme-svg/primebot)", color=0x282828)
         embedHelp.set_footer(text="Created by PrimeTime09#1847, Running on Heroku")
         await ctx.send(embed=embedHelp)
 
