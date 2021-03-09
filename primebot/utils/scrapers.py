ff7f7f2a5f1f11cb16a7d5d0c5263142db5f8337
pryme-svg <edoc.www@gmail.com>
2021-03-08 17:15:53 -0800

    syntax fixes for utils.scrapers and fix about cmd

--- primebot/utils/scrapers.py
+++ primebot/utils/scrapers.py
@@ -1,15 +1,15 @@
-from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
 import os
 import re
 
+
 def scrape_song_lyrics(url):
     page = requests.get(url)
     html = BeautifulSoup(page.text, 'html.parser')
-    lyrics =html.find('div', class_='lyrics').get_text()
-    #remove identifiers like chorus, verse, etc
+    lyrics = html.find('div', class_='lyrics').get_text()
+    # remove identifiers like chorus, verse, etc
     lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
-    #remove empty lines
-    lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])         
-    return lyrics 
+    # remove empty lines
+    lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])
+    return lyrics
