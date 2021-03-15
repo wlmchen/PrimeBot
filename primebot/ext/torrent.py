29fcf37e0aef13540529e862fa36232c9bbea699
pryme-svg <edoc.www@gmail.com>
2021-03-14 18:11:05 -0700

    query not found for torrents

--- primebot/ext/torrent.py
+++ primebot/ext/torrent.py
@@ -12,11 +12,13 @@ class Torrent(commands.Cog):
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def torrent(self, ctx, query):
         async with ctx.channel.typing():
-            url='https://torrent-api1.herokuapp.com/getTorrents?site=1337x&query={}'.format(query)
+            url = 'https://torrent-api1.herokuapp.com/getTorrents?site=1337x&query={}'.format(query)
             r = requests.get(url)
             json = r.json()
             embed = discord.Embed(description=ctx.message.author.mention, color=0xD63600)
             embed.set_author(name=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+            if not json['torrents']:
+                await ctx.send("Query not found")
             for torrent in json['torrents']:
                 desc = "**[magnet]({})** | **[1337]({})** | Seeds: {} | Leeches: {} | Size: {}".format(torrent['shortlink'], torrent['link'], torrent['seeds'], torrent['leeches'], torrent['size'])
                 embed.add_field(name=torrent['name'], value=desc, inline=False)
