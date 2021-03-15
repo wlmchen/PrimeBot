d0b82dd2132bd3f03a1d3350ee9c30400c4a44cf
William Chen <wlmchen16@gmail.com>
2021-03-14 18:07:45 -0700

    torrent command

---
+++ primebot/ext/torrent.py
@@ -0,0 +1,27 @@
+import discord
+import requests
+from discord.ext import commands
+
+
+class Torrent(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.cooldown(1, 3, commands.BucketType.user)
+    async def torrent(self, ctx, query):
+        async with ctx.channel.typing():
+            url='https://torrent-api1.herokuapp.com/getTorrents?site=1337x&query={}'.format(query)
+            r = requests.get(url)
+            json = r.json()
+            embed = discord.Embed(description=ctx.message.author.mention, color=0xD63600)
+            embed.set_author(name=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+            for torrent in json['torrents']:
+                desc = "**[magnet]({})** | **[1337]({})** | Seeds: {} | Leeches: {} | Size: {}".format(torrent['shortlink'], torrent['link'], torrent['seeds'], torrent['leeches'], torrent['size'])
+                embed.add_field(name=torrent['name'], value=desc, inline=False)
+            await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Torrent(bot))
