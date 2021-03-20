1b5c719836710c587a61f38e7c02759a0b51c76a
William Chen <wlmchen16@gmail.com>
2021-03-20 16:13:00 -0700

    giveup command for who game

--- primebot/__init__.py
+++ primebot/__init__.py
@@ -5,30 +5,12 @@ import discord
 from discord.ext import commands
 
 
-async def get_prefix(bot, message):
-    try:
-        guild_id = message.guild.id
-    except AttributeError:  # errors if in dm
-        prefix = primebot.conf['prefix']
-        return commands.when_mentioned_or(prefix)(bot, message)
-    # if guild doesn't exist in db
-    if primebot.db.prefixes.find_one({'guild_id': guild_id}) is None:
-        new = {
-            "guild_id": guild_id,
-            "guild_name": message.guild.name,
-            "prefix": primebot.conf['prefix']
-        }
-        primebot.db.prefixes.insert_one(new)
-    prefix = primebot.db.prefixes.find_one({"guild_id": guild_id})['prefix']
-    return commands.when_mentioned_or(prefix)(bot, message)  # allow ping as prefix
-
-
 class PrimeBot(commands.Bot):
 
     def __init__(self):
         allowed_mentions = discord.AllowedMentions(roles=False, everyone=False, users=True)
         intents = discord.Intents.all()  # enable all intents
-        super().__init__(command_prefix=get_prefix,
+        super().__init__(command_prefix=self.get_prefix,
                          intents=intents,
                          allowed_mentions=allowed_mentions
                          )
@@ -37,6 +19,23 @@ class PrimeBot(commands.Bot):
 
         for ext in primebot.conf['exts']:
             self.load_extension('primebot.ext.{}'.format(ext))
+
+    async def get_prefix(bot, message):
+        try:
+            guild_id = message.guild.id
+        except AttributeError:  # errors if in dm
+            prefix = primebot.conf['prefix']
+            return commands.when_mentioned_or(prefix)(bot, message)
+        # if guild doesn't exist in db
+        if primebot.db.prefixes.find_one({'guild_id': guild_id}) is None:
+            new = {
+                "guild_id": guild_id,
+                "guild_name": message.guild.name,
+                "prefix": primebot.conf['prefix']
+            }
+            primebot.db.prefixes.insert_one(new)
+        prefix = primebot.db.prefixes.find_one({"guild_id": guild_id})['prefix']
+        return commands.when_mentioned_or(prefix)(bot, message)  # allow ping as prefix
 
     def check_blacklist(self, ctx):
         if ctx.author.id in primebot.conf["blacklist"]:
