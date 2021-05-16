33a3c05ae3322e57928b66dd71a26915a55723c7
pryme-svg <edoc.www@gmail.com>
2021-05-16 13:24:28 -0700

    various fixes

--- primebot/ext/torrent.py
+++ primebot/ext/torrent.py
@@ -71,7 +71,7 @@ class Torrent(commands.Cog):
             embed.timestamp = datetime.datetime.utcnow()
             await ctx.send(embed=embed)
 
-    @commands.command()
+    @commands.command(hidden=True)
     @is_pt()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def sows(self, ctx, *, query):
@@ -92,7 +92,7 @@ class Torrent(commands.Cog):
             embed.add_field(name=torrent['name'], value=desc, inline=False)
         await ctx.send(embed=embed)
 
-    @commands.command()
+    @commands.command(hidden=True)
     @is_pt()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def ops(self, ctx, *, query):
